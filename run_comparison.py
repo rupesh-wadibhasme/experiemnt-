e72e4c89e2a23f49178c3807e2e80329abb8bb3e
Rupesh Wadibhasme <rupesh.wadibhasme@gmail.com>
2025-04-04 16:46:07 +0530

    Create run_comparison.py

---
+++ run_comparison.py
@@ -0,0 +1,10 @@
+from docx_loader_comparison import DocxLoaderComparison
+
+# Path to your test Word file
+test_file = "sample.docx"
+
+# Compare Performance
+DocxLoaderComparison.compare_performance(test_file)
+
+# Compare Accuracy
+DocxLoaderComparison.compare_accuracy(test_file)
