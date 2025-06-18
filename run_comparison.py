331f6bc784f0a4b289719ed11035bc107913e090
Rupesh Wadibhasme <rupesh.wadibhasme@gmail.com>
2025-06-18 10:49:02 +0530

    Delete run_comparison.py

--- run_comparison.py
+++
@@ -1,7 +0,0 @@
-from docx_loader_comparison import DocxLoaderComparison
-
-# Path to your test Word file
-test_file = "sample.docx"
-
-# Run Evaluation
-DocxLoaderComparison.run_evaluation(test_file)
